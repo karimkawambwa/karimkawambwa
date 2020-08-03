d4174df726a1efb275395496c502f2ec2768e469
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2020-08-03 12:03:02 -0230

    Added gh-pages

--- site/src/components/header.js
+++ site/src/components/header.js
@@ -1,19 +1,29 @@
 import React from "react"
 import Link from "./link"
-import { Box } from "@material-ui/core"
+import { Box, makeStyles } from "@material-ui/core"
 
-const Header = () => (
-  <Box component="header" marginBottom="2rem">
-    <Box component="nav" display="flex">
-      <Link href="/">Karim Kawambwa ( Home )</Link>
-      <Box margin="0 10px">-</Box>
-      <Link href="/blog">Blog</Link> 
-      <Box margin="0 10px">-</Box>
-      <Link href="/projects">Projects</Link> 
-      <Box margin="0 10px">-</Box>
-      <Link href="/about">About</Link>
+const useStyles = makeStyles({
+  nav: {
+    display: "flex"
+  }
+});
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <Box component="header" marginBottom="2rem">
+      <Box component="nav" className={classes.nav}>
+        <Link href="/">Karim Kawambwa ( Home )</Link>
+        <Box margin="0 10px">-</Box>
+        <Link href="/blog">Blog</Link> 
+        <Box margin="0 10px">-</Box>
+        <Link href="/projects">Projects</Link> 
+        <Box margin="0 10px">-</Box>
+        <Link href="/about">About</Link>
+      </Box>
     </Box>
-  </Box>
-)
+  )
+}
 
 export default Header
